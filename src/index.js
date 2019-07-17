0ebf20ce4480cd7b51c26f09a14da2831798df6e
yonmey <yonmey@gmail.com>
2019-07-18 00:32:21 +0200

    invoke cron on init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import sendTelegram from './telegram'
 import { CronJob } from 'cron'
 
-const init = () =>
-  new CronJob('0 0 8 * * *', () => sendTelegram(), null, true, 'Europe/Paris')
-
-init()
+(new CronJob('0 0 8 * * *', () => sendTelegram(), null, true, 'Europe/Paris'))
